1d72a7801d7b714fd6f12700ec1cde9b541e1dd2
lms <lms3082@outlook.com>
2025-06-22 11:31:41 +0800

    2-test

--- src/test.java
+++ src/test.java
@@ -1,6 +1,21 @@
 public class test {
     public static void main(String[] args) {
-        double rand=Math.random();
-        System.out.println(rand);
+        //掷骰子游戏 三次大于15 手气不错 10-15 手气一般 3-9 不怎么样
+        int i=(int)(Math.random()*6+1);
+        int j=(int)(Math.random()*6+1);
+        int k=(int)(Math.random()*6+1);
+        int sum=i+j+k;
+        System.out.println("第一次掷骰子:"+i);
+        System.out.println("第一次掷骰子:"+j);
+        System.out.println("第一次掷骰子:"+k);
+        if(sum>15) {
+            System.out.println("手气不错");
+        }
+        else if(sum>=10&&sum<15) {
+            System.out.println("手气一般");
+        }
+        else {
+            System.out.println("手气太差");
+        }
     }
 }
